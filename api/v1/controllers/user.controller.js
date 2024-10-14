e460cc080611870aaa239c0c664b73a90eca4c65
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-14 20:22:49 +0700

    fix token
    vi no ko load lai trang nen la token  giong nhau tat

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -28,7 +28,8 @@ module.exports.register = async (req, res) => {
     const user = new User({
       fullName: req.body.fullName,
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      token: generateHelper.generateRandomString(30)
     })
 
     user.save() //-sau khi save song thi tra cho cai token
